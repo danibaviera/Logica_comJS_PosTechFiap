634eb45db195fc4d651a059dbbe6670d56fce207
Danieli Baviera <85691671+danibaviera@users.noreply.github.com>
2024-05-26 19:55:16 -0300

    Add files via upload

---
+++ Logica_com_soma_e_divisao.js
@@ -0,0 +1,7 @@
+let a = 1.5;
+let b = 0.5;
+let c = (a+b)*2 + 1.5*2;
+
+console.log(c);
+
+//let c = 7 pq? primeiro fazz o que está entre (),  após as divisões e após as somas. Ficam a+b = 2*2 = 4 | 1.5* 2 = 3 | 4 + 3 = 7 
