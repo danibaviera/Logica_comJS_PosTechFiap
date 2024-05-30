6ae9cb62b3c66689b1a9bbc30ec3e75dd43d87e4
Danieli Baviera <85691671+danibaviera@users.noreply.github.com>
2024-05-30 18:42:54 -0300

    Add files via upload

---
+++ Vetores_e_Matrizes.js
@@ -0,0 +1,39 @@
+//Vetores e Matrizes
+//Implementando Array no JS 
+
+//Escrevendo variável que guarda um grupo
+
+let a = [10, 5, 8];
+console.log (a); 
+
+// Alterando a posição do vetor: 
+a [2] = 9; 
+
+// Lembrando que tudo na programação começa do zero, logo o número 8 está na posição 2 e ele vai mudar para o número 9 
+console.log (a);
+
+// Lendo vetor na posição zero: 
+let b = a[0];
+let c = a[0];
+console.log (b);
+console.log (c);
+
+//descobrindo a quantidade de um vetor: 
+let a = [10,5,8];
+let qtd = a.length;
+console.log (qtd);3
+
+// Iterando um vetor 
+let a = [10,5,8];
+for (let cont = 0; cont <)
+for (let cont = 0; cont < a.length; cont++) {
+} //Estratégia de repetição quando nao sabe o valor final
+
+let a = [10,5,8];
+for (let cont = 0; cont<a.length; cont++) {
+	let item = a[cont];
+	console.log(item);
+} 
+
+
+
